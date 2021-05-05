5278d1fb8d39dede00506eef42c6b376cfc56fd0
Dec Kolakowski <declankolakowski@gmail.com>
2021-05-05 22:15:51 +0100

    Update syntax parsing

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,59 @@ use regex::Regex;
 use std::collections::HashMap;
 
 fn main() {
-    tokenize("SELECT SERVICE WHERE port = 443");
+    let tokens = tokenize("SELECT SERVICE WHERE port = 443");
+    if tokens.iter().any(|token| match token { TokenType::InvalidSyntax(_) => true, _ => false }) {
+        tokens
+            .iter()
+            .enumerate()
+            .for_each(|(index, invalid)| {
+                match invalid {
+                    TokenType::InvalidSyntax(syntax) => {
+                        println!("Invalid syntax \"{}\" at token position {}.", syntax, index)
+                    },
+                    _ => ()
+                }
+            })
+    } else {
+        println!("{:?}", tokens)
+    }
 }
 
 fn tokenize(input: &str) -> Vec<TokenType> {
     input
         .split_whitespace()
-        .map(|raw_element| {
-            match attempt_token(raw_element) {
-                Some(token) => token,
-                None => TokenType::InvalidSyntax
-            }
-        })
+        .map(|raw_element| attempt_token(raw_element))
         .collect()
 }
 
-fn attempt_token(input: &str) -> Option<TokenType> {
-    let regexes: Vec<Regex> = vec![
-        Regex::new("^And$").unwrap(),
-        Regex::new("[a-z0-9]+").unwrap(),
-        Regex::new("^CONTAINS$").unwrap(),
-        Regex::new("^=$").unwrap(),
-        Regex::new("^SELECT$").unwrap(),
-        Regex::new("^WHERE$").unwrap(),
-        Regex::new("^ROUTE$|SERVICE|ALL|GLOBAL$").unwrap(),
-    ];
+fn attempt_token(input: &str) -> TokenType {
+    let mut tokens: HashMap<&str, TokenType> = HashMap::new();
+    tokens.insert("^SELECT$", TokenType::Select);
+    tokens.insert("^AND$", TokenType::And);
+    tokens.insert("^CONTAINS$", TokenType::Contains);
+    tokens.insert("^WHERE$", TokenType::Where);
+    tokens.insert("^ROUTE$", TokenType::Level(ConfigLevel::Route));
+    tokens.insert("^SERVICE$", TokenType::Level(ConfigLevel::Service));
+    tokens.insert("^ALL$", TokenType::Level(ConfigLevel::All));
+    tokens.insert("^GLOBAL$", TokenType::Level(ConfigLevel::Global));
+    tokens.insert("^=$", TokenType::Equals);
+    tokens.insert("^[a-z0-9]+$", TokenType::StringValue(String::new()));
 
-    match regexes
-        .iter()
-        .find(|regex| regex.is_match(input))
-        .map(|regex| regex.as_str())
-    {
-        Some("^AND$") => Some(TokenType::And),
-        Some("[a-z0-9]+") => Some(TokenType::StringValue(String::from(input))),
-        Some("^CONTAINS$") => Some(TokenType::Contains),
-        Some("^=$") => Some(TokenType::Equals),
-        Some("^SELECT$") => Some(TokenType::Select),
-        Some("^WHERE$") => Some(TokenType::Where),
-        Some("^ROUTE$|SERVICE|ALL|GLOBAL$") => {
-            match input {
-                "GLOBAL" => Some(TokenType::Level(ConfigLevel::Global)),
-                "SERVICE" => Some(TokenType::Level(ConfigLevel::Service)),
-                "ROUTE" => Some(TokenType::Level(ConfigLevel::Route)),
-                _ => Some(TokenType::Level(ConfigLevel::All))
-            }
-        },
-        _ => None
+    let regexes: Vec<Regex> = tokens.keys().map(|regex| Regex::new(regex).unwrap()).collect();
+
+    let token_key: &str = match regexes.iter().find(|regex| regex.is_match(input)) {
+        Some(regex) => regex.as_str(),
+        None => ""
+    };
+
+    match tokens.get(token_key) {
+        Some(TokenType::StringValue(_)) => TokenType::StringValue(String::from(input)),
+        Some(token) => token.clone(),
+        None => TokenType::InvalidSyntax(String::from(input))
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     Level(ConfigLevel),
     StringValue(String),
@@ -60,10 +63,10 @@ enum TokenType {
     Equals,
     Select,
     Where,
-    InvalidSyntax
+    InvalidSyntax(String)
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum ConfigLevel {
     Service,
     Route,
@@ -77,14 +80,19 @@ mod test {
 
     #[test]
     fn test_tokenizer() {
-        let tokens = tokenize("SELECT SERVICE WHERE port = 443");
+        let tokens = tokenize("SELECT SERVICE WHERE port = 443 AND protocol CONTAINS get post");
         let exp_tokens = vec![
             TokenType::Select,
             TokenType::Level(ConfigLevel::Service),
             TokenType::Where,
             TokenType::StringValue(String::from("port")),
             TokenType::Equals,
-            TokenType::StringValue(String::from("443"))
+            TokenType::StringValue(String::from("443")),
+            TokenType::And,
+            TokenType::StringValue(String::from("protocol")),
+            TokenType::Contains,
+            TokenType::StringValue(String::from("get")),
+            TokenType::StringValue(String::from("post"))
         ];
         assert_eq!(tokens, exp_tokens)
     }
